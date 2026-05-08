f290e0307b21f4e9cd092a0998ae9a89ed1d01c0
vbncursed <enrbyltw@gmail.com>
2026-05-08 09:11:52 +0500

    Initial Commit

---
+++ Monopoly Tracker/Monopoly_TrackerApp.swift
@@ -0,0 +1,32 @@
+//
+//  Monopoly_TrackerApp.swift
+//  Monopoly Tracker
+//
+//  Created by vbncursed on 8/5/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Monopoly_TrackerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Monopoly TrackerTests/Monopoly_TrackerTests.swift
@@ -0,0 +1,19 @@
+//
+//  Monopoly_TrackerTests.swift
+//  Monopoly TrackerTests
+//
+//  Created by vbncursed on 8/5/26.
+//
+
+import Testing
+@testable import Monopoly_Tracker
+
+struct Monopoly_TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
